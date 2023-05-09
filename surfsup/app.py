f6c17426cc1984b68d38b5759e55feb31ad7eebd
kristen1286 <krpollok98@gmail.com>
2023-05-08 22:50:28 -0500

    updating the app.py

--- surfsup/app.py
+++ surfsup/app.py
@@ -23,7 +23,7 @@ Measurement = Base.classes.measurement
 Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-session = Session(engine)
+session = Session(bind=engine)
 
 #################################################
 # Flask Setup
